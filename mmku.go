f11a06e1787ffc14bb0687a1d623ebf6991f7497
cyian-1756 <devnull64@vfemail.net>
2018-12-27 01:54:33 -0500

    Now handles the edge cases of the settings file not exiting or the app_Key key not existing

--- mmku.go
+++ mmku.go
@@ -25,6 +25,7 @@ func getHomeDir() string {
 
 func ExampleScrape() {
 	// Request the HTML page.
+	fmt.Println("[*] Getting webpage")
 	res, err := http.Get("https://www.makemkv.com/forum/viewtopic.php?f=5&t=1053")
 	if err != nil {
 		log.Fatal(err)
@@ -41,27 +42,42 @@ func ExampleScrape() {
 	}
 
 	// Find the new key
+	fmt.Println("[*] Extracting key")
 	key := doc.Find(".codebox pre code").Text()
 	newFile := ""
-	dat, err := ioutil.ReadFile(getHomeDir() + "/.MakeMKV/settings.conf")
-	f := strings.Split(string(dat), "\n")
-	for _, element := range f {
-		if strings.HasPrefix(element, "app_Key") {
-			currentKey := strings.Replace(element, "app_Key", "", -1)
-			currentKey = strings.Replace(currentKey, " ", "", -1)
-			currentKey = strings.Replace(currentKey, "=", "", -1)
-			currentKey = strings.Replace(currentKey, "\"", "", -1)
-			if currentKey == key {
-				fmt.Println("[*] Already using newest key")
-				return
-			}
-
-			fmt.Println("[*] Updating key")
-			newFile += "app_Key = \"" + key + "\""
-
-		} else {
-			newFile += element + "\n"
+	fileContainsAppKey := false
+	// If the file doesn't exist we skip reading it and set it to it's default values
+	if _, err := os.Stat("/path/to/whatever"); os.IsNotExist(err) {
+		newFile = "#\n# MakeMKV settings file, written by MakeMKV v1.10.8 linux(x64-release)\n#\napp_BackupDecrypted = \"1\"\n"
+	} else {
+		dat, err := ioutil.ReadFile(getHomeDir() + "/.MakeMKV/settings.conf")
+		if err != nil {
+			log.Fatal(err)
 		}
+		f := strings.Split(string(dat), "\n")
+		for _, element := range f {
+			if strings.HasPrefix(element, "app_Key") {
+				fileContainsAppKey = true
+				currentKey := strings.Replace(element, "app_Key", "", -1)
+				currentKey = strings.Replace(currentKey, " ", "", -1)
+				currentKey = strings.Replace(currentKey, "=", "", -1)
+				currentKey = strings.Replace(currentKey, "\"", "", -1)
+				if currentKey == key {
+					fmt.Println("[*] Already using newest key")
+					return
+				}
+
+				fmt.Println("[*] Updating key")
+				newFile += "app_Key = \"" + key + "\""
+
+			} else if element != "\n" {
+				newFile += element + "\n"
+			}
+		}
+	}
+	// If fileContainsAppKey is false then the settings file did not contain a app_Key key and we append it
+	if !fileContainsAppKey {
+		newFile += "app_Key = \"" + key + "\""
 	}
 	fmt.Println("[*] Writing file")
 	ioutil.WriteFile(getHomeDir()+"/.MakeMKV/settings.conf", []byte(newFile), 0644)
